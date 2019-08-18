070242f4e6de3427ca6bdf5d079fd5444b7ec13b
Alex Mojnov <alexmojnov@gmail.com>
2019-08-18 15:50:32 -0400

    initial logic

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Charlie",
+		photo: "https://picsum.photos/200",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Sweet Dee",
+		photo: "https://picsum.photos/200",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Dennis",
+		photo: "https://picsum.photos/200",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Mac",
+		photo: "https://picsum.photos/200",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Frank",
+		photo: "https://picsum.photos/200",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
